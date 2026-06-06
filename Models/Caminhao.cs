a83133bae5635f9f684c7c8e3c4105db00b93418
habnner <habnner.test@gmail.com>
2026-06-06 17:11:37 -0300

    Update Caminhao.cs

--- Models/Caminhao.cs
+++ Models/Caminhao.cs
@@ -2,11 +2,6 @@ using Estacionamento.Enums;
 
 namespace Estacionamento.Models {
 
-    /*
-        Representa um caminhão.
-        Valor da hora: R$ 18. Possui taxa de carga: R$ 15.
-    */
-
     public class Caminhao : Veiculo {
 
         public override decimal ValorHora => 18m;
